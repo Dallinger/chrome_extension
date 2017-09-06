0e11d59182ade36860174ea3f744ce275a6c24e0
Alec Mitchell <alecpm@gmail.com>
2017-09-06 11:13:37 -0700

    Safety first.

--- src/inject/inject-mitm.js
+++ src/inject/inject-mitm.js
@@ -6,7 +6,9 @@ chrome.extension.sendMessage({type: 'ping'}, function(response) {
             var experiment_url = (vals.experiment_url || '').replace(/\/$/, "");
             var participant_id = vals.participant_id;
             var script1 = document.createElement("script");
-            script1.innerHTML = "window.dlgr = window.dlgr || {};\ndlgr.experiment_url = '" + experiment_url + "';\ndlgr.participant_id = '" + participant_id + "';";
+            script1.innerHTML = "window.dlgr = window.dlgr || {};\ndlgr.experiment_url = " +
+                                JSON.stringify(experiment_url) + ";\ndlgr.participant_id = " +
+                                JSON.stringify(participant_id) + ";";
             document.head.appendChild(script1);
             var script2 = document.createElement('script');
             script2.setAttribute('type','text/javascript');
